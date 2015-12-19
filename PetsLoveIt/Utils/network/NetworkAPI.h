2dbd0a62d1cff0c86ed3471267c1ab138b57a73c
kongjun <kongkong888123@gmail.com>
2015-12-19 14:41:21 +0800

    修改接口服务器

--- PetsLoveIt/Utils/network/NetworkAPI.h
+++ PetsLoveIt/Utils/network/NetworkAPI.h
@@ -8,7 +8,7 @@
 
 #ifndef iOSAppFramework_NetworkAPI_h
 #define iOSAppFramework_NetworkAPI_h
-#define kBaseURL                    @"http://121.42.51.60:8088/petweb/actions/"
+#define kBaseURL                    @"http://61.155.210.59:8088/petweb/actions/"
 #define UMENG_APPKEY                @"56484222e0f55ad4db009b90"
 #define UMENG_channelId             @"petiOSAppStore"
 
