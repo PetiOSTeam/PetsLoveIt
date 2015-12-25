80a1b166d1a20882055a6a5652d399dd6771cec3
kongjun <kongkong888123@gmail.com>
2015-12-25 10:07:41 +0800

    commit

--- PetsLoveIt/me/controller/SysMsgViewController.m
+++ PetsLoveIt/me/controller/SysMsgViewController.m
@@ -18,7 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self config];
-    
+    [self setMsgRead];
+}
+
+- (void)setMsgRead{
+    NSDictionary *params = @{@"uid":@"saveUserReadMsg",
+                             @"msgType":@"1"
+                             };
+    [APIOperation GET:@"common.action" parameters:params onCompletion:^(id responseData, NSError *error) {
+        if (!error) {
+            
+        }
+    }];
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
