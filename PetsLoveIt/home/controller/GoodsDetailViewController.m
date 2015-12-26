1553a45f5410477b57886f37568e63fd5c026412
kongjun <kongkong888123@gmail.com>
2015-12-26 23:49:36 +0800

    fix bug

--- PetsLoveIt/home/controller/GoodsDetailViewController.m
+++ PetsLoveIt/home/controller/GoodsDetailViewController.m
@@ -362,7 +362,7 @@
 #pragma mark - 加载HTML页面数据，并进行图文混排
 -(void) loadViewAndData{
     NSString *html = self.goods.prodDetail;
-    CGFloat viewwidth = [UIScreen mainScreen].bounds.size.width - 40;
+    CGFloat viewwidth = [UIScreen mainScreen].bounds.size.width - 24;
     NSString *css = [NSString stringWithFormat:@"<html><meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no\" /><body width=%f style=\"word-wrap:break-word;ext-align: justify; font-family:Arial\"><style>img{max-width:%f;height:auto;}</style>",viewwidth,viewwidth];
     
     NSMutableString *desc = [NSMutableString stringWithFormat:@"%@%@%@",
